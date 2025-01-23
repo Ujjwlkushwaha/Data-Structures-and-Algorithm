10e53dc7afef8d833950e0df16cc33a21345f116
ujjwlkushwaha <ukeditz71@gmail.com>
2025-01-23 12:31:30 +0530

    GFG Solution ( hard )

--- 08-LinkedList/Codehelp/03_Questions/09_cloneLinkedlist/cloneLinkedlist.cpp
+++ 08-LinkedList/Codehelp/03_Questions/09_cloneLinkedlist/cloneLinkedlist.cpp
@@ -1,27 +1,110 @@
-// another leetcode problem
-
-//  clone linkedlist with  random pointer
-
 /*
-    public:
-        int val;
-        Node* next;
-        Node* random;
+    ✅✅ Clone List with Next and Random 📌 ( Difficulty: Hard )
 
-        Node(int _val) {
-            val = _val;
-            next = NULL;
-            random = NULL;  this pointer pointes any random node of the linkedlist
-        }
-};
 
-Hme issi  list ki copy create krni hai and jaise iss list me node jaha jiske sath add hogi usme bhi vaise hi add krni hai
+        You are given a special linked list with n nodes where each node has two pointers-
+            a next pointer that points to the next node of the singly linked list,
+            a random pointer that points to the random node of the linked list.
+
+        Construct a copy of this linked list.
+             The copy should consist of the same number of new nodes, where each new node has the value corresponding to its original node.
+
+             Both the next and random pointer of the new nodes should point to new nodes in the copied list, such that it also represent the same list state. None of the pointers in the new list should point to nodes in the original list.
+
+        Return the head of the copied linked list.
+
+        NOTE : Original linked list should remain unchanged.
 */
 
-
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+class Node
+{
+
+public:
+    int data;
+    Node *next;
+    Node *random;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+        random = NULL;
+    }
+};
+
+// ✅ Method 1:  tc ->  O(n) sc-> O(n)   using map
+Node *helper(Node *head, unordered_map<Node *, Node *> mp)
+{
+    if (!head)
+        return NULL;
+
+    Node *newNode = new Node(head->data);
+    mp[head] = newNode;
+
+    newNode->next = helper(head->next, mp);
+
+    if (head->random)
+    {
+        newNode->random = mp[head->random];
+    }
+    return newNode;
+}
+
+Node *cloneLinkedList1(Node *head)
+{
+    // Write your code here
+    unordered_map<Node *, Node *> mp;
+    return helper(head, mp);
+}
+
+
+
+
+// ✅ method 2: tc-> O(n)  sc-> O(1)
+Node *cloneLinkedList2(Node *head)
+{
+    // Write your code here
+    if (head == NULL)
+        return head;
+
+    Node *temp = head;
+    while (temp)
+    {
+        Node *newNode = new Node(temp->data);
+        newNode->next = temp->next;
+        temp->next = newNode;
+        temp = newNode->next;
+    }
+
+    temp = head;
+    Node *head2 = temp->next;
+    while (temp != NULL)
+    {
+        if (temp->random == NULL)
+        {
+            temp->next->random = NULL;
+        }
+        else
+        {
+            temp->next->random = temp->random->next;
+        }
+
+        temp = temp->next->next;
+    }
+
+    temp = head;
+    while (temp != NULL)
+    {
+        Node *temp2 = temp->next;
+        temp->next = temp2->next;
+
+        if (temp2->next)
+            temp2->next = temp2->next->next;
+        temp = temp->next;
+    }
+
+    return head2;
 }
