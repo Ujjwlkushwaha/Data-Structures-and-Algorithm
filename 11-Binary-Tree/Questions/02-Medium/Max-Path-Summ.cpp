6ecaa777fcb202eafbabb19d50f3f38b462458fd
ujjwlkushwaha <ukeditz71@gmail.com>
2025-02-09 22:38:47 +0530

    Add new Question

---
+++ 11-Binary-Tree/Questions/02-Medium/Max-Path-Summ.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+int getMaxSum( Node* root , int &maxSum)
+{
+    // base case
+    if( root == NULL ) return 0;
+
+    // get sum of left subtree
+    int l = max( 0 , getMaxSum( root->left , maxSum));
+
+    // get sum of right subTree
+    int r = max( 0 , getMaxSum( root->right , maxSum));
+
+    // store maxSum 
+    maxSum = max( maxSum , l + r + root->data );
+
+    // Return the maximum path sum rooted at this node
+    return root->data + max( l , r);
+}
+
+int maxPathSum(Node *root)
+{
+    int sum = root->data; // store max sum
+    getMaxSum( root , sum );
+}
